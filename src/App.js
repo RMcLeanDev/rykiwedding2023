d49b706b2564115072c01306469bc15a07c9908f
RMcLeanDev <rmcleandev@gmail.com>
2022-09-26 19:18:30 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <h1>Kina & Ryan</h1>
+      <img src={require('./assets/wedpic.jpeg')}/>
+    </div>
+  );
+}
+
+export default App;
